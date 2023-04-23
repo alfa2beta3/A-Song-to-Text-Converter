979208f68a93e66bb103d7b497a23950683cb85c
alfa2beta3 <leexingyang180@gmail.com>
2023-04-23 21:49:50 +0800

    add 3 songs: Ignite by Alan Walker, Monster by YOASOBI, and Deja Vu by Dave Rodgers, add whisper2 and whisper2.py

---
+++ whisper2.py
@@ -0,0 +1,28 @@
+import whisper
+import os
+import numpy as np
+import torch
+
+
+torch.cuda.is_available()
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = whisper.load_model("base", device=DEVICE)
+print(
+    f"Model is {'multilingual' if model.is_multilingual else 'English-only'} "
+    f"and has {sum(np.prod(p.shape) for p in model.parameters()):,} parameters."
+)
+
+audio = whisper.load_audio("deja_vu.mp3")
+audio = whisper.pad_or_trim(audio)
+mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+_, probs = model.detect_language(mel)
+print(f"Detected language: {max(probs, key=probs.get)}")
+
+options = whisper.DecodingOptions(language="en", without_timestamps=True, fp16 = False)
+result = whisper.decode(model, mel, options)
+print(result.text)
+
+result = model.transcribe("deja_vu.mp3")
+print(result["text"])
